740967fa1433e78749d59b3c4ceb6eafa66f65cb
Polish Coder <polygonssoftware@gmail.com>
2023-05-04 19:37:17 +0200

    Create DontDestroyOnLoad class

---
+++ Scripts/Utils/DontDestroyOnLoad.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace UniversalUnityPackage
+{
+    public class DontDestroyOnLoad : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
